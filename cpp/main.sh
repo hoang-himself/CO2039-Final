bad60aaf1d4e4a18bad17a3a48f54fcdab9c5927
Hoang NGUYEN <baauco@gmail.com>
2021-08-20 01:17:17 +0700

    Modular inputminted instead of hardcoding

--- cpp/main.sh
+++ cpp/main.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -f main && g++ -g main.cpp -o main && ./main
+g++ -g main.cpp -o main && ./main > main.txt && rm -f main
--- hs/main.sh
+++ hs/main.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -f main && ghc -g main.hs -o main && rm -f main.{hi,o} && ./main
+ghc -g main.hs -o main && rm -f main.{hi,o} && ./main > main.txt && rm -f main
